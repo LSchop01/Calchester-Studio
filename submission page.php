e1801fafed6e9936258bd68431d8e3881a07d1bb
LSchop01 <Lschop01@my.blueridgectc.edu>
2020-04-15 22:46:07 -0400

    added a php

---
+++ submission page.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>Calchester Studio</title>
+	<meta charset="utf-8">
+			<link rel="stylesheet" href="style sheet.css">
+			<link rel="icon" type="image/png" href="images/personal/favicon.png"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="School Project Art Portfolio Website">
+	<meta name="keywords" content="Art Portfolio, Portfolio, Digital Art, Digital Illustrator">
+	<meta name="author" content="Lillie Schoppert">
+</head>
+
+<body>
+	<div id= "wrapper">
+	
+<header>
+	<div class="header nav">
+		<h1>Calchester Studio</h1>
+		<nav>
+			<a href="index.html">Home</a> &nbsp;
+			<a href="contact page.html">Contact</a> &nbsp;
+			<a href="gallery page.html">Familiarity Gallery</a> &nbsp;
+		</nav>
+	</div>
+</header>
+
+<main>
+	<h2>Thank you!</h2>
+		<p>I'll be in touch with you soon!</p>
+
+<footer>
+    <a href="mailto:Lillie@Schoppert.com">Lillie@Schoppert.com</a>
+</footer>
+
+</div>
+</body>
+
+</html>
